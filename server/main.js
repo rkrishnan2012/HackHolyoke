74a14405fcbb1cd0a2b12516d0bea6fbae5ab18a
Rohit Krishnan <rkrishnan@hivelabs.it>
2015-04-07 01:00:21 -0400

    blah

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ Meteor.methods({
     saveFile: function(blob){
         var buf = new Buffer(blob, 'base64');
         var fs = Npm.require('fs');
-        var wstream = fs.createWriteStream('~/face.jpg');
+        var wstream = fs.createWriteStream('/tmp/face.jpg');
         wstream.write(buf);
         wstream.end();
         console.log("File was saved");
@@ -23,14 +23,14 @@ function checkEmotion() {
 
     var require = Npm.require;
     var spawn = require('child_process').spawn;
-    var emotime = spawn("~/emotime/assets/runEmotime.sh", []);
+    var emotime = spawn("/emotime/assets/runEmotime.sh", []);
 
     emotime.on('close', function(code) {
         fut.return();
     });
     fut.wait();
     var fs = Npm.require('fs');
-    var data = fs.readFileSync('~/emotime/output.json', 'utf8');
+    var data = fs.readFileSync('/emotime/output.json', 'utf8');
     var emotionjson = JSON.parse(data);
     console.log("Finished the analysis!");
     console.log(emotionjson);
